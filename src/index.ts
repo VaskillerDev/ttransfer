f18e6ba4e8a0b426138697d546584464d4ed51fd
VaskillerDev <Vaskoooo9241@gmail.com>
2020-10-09 06:31:01 +0300

    update package dependency by token

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,20 @@
 // main codepoint
+import {ConsoleLogger, Http2ServerConnection, ILogger, Logger, Server} from "ttransfer_util";
+import {IncomingMessage, ServerResponse} from "http";
+
+type Protocol = "http" | "https";
+type ImplServerConnection<T> = Http2ServerConnection<T> | Http2ServerConnection<T>;
+
 (() => {
-  /*  const fileLogger: ILogger = new ConsoleLogger();
-  const logger = new Logger(fileLogger);
-  const serverCore = new HttpServerCore(logger);
-  serverCore.create(9090);*/
+    const fileLogger: ILogger = new ConsoleLogger();
+    const logger = new Logger(fileLogger);
+    const protocol : Protocol = "http";
+    const server = new Server(logger,protocol,"localhost",9090);
+    server.on("/f",(req : IncomingMessage,res : ServerResponse)=>{
+        logger.log("ip: " + req.connection.remoteAddress || "none");
+        res.statusCode = 200;
+        res.end();
+    })
+
+
 })();
