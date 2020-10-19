58aaf5992fe745da78bd657b5373682410c903fb
VaskillerDev <Vaskoooo9241@gmail.com>
2020-10-19 03:51:02 +0300

    +todo

--- src/server/Route/SecurityRoute.ts
+++ src/server/Route/SecurityRoute.ts
@@ -6,7 +6,7 @@ type Method = "post" | "get" | "put" | "delete";
 class SecurityRoute extends BasicRoute {
   constructor(url: string | symbol, method: Method) {
     super(url, method);
-    AllowListRoute.add(url as string);
+    AllowListRoute.add(url as string); // todo: rewrite later - singleton in object
   }
 }
 
