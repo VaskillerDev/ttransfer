f808c2277a8a3eb16fffc61a708689213d786453
VaskillerDev <Vaskoooo9241@gmail.com>
2020-11-09 22:02:23 +0300

    minor

--- src/server/Route/SecurityRoute.ts
+++ src/server/Route/SecurityRoute.ts
@@ -1,12 +1,12 @@
 import { BasicRoute } from "ttransfer_util";
-import AllowListRoute from "./AllowListRoute";
+import AllowListForRoutes from "./AllowListForRoutes";
 
 type Method = "post" | "get" | "put" | "delete";
 
 class SecurityRoute extends BasicRoute {
   constructor(url: string | symbol, method: Method) {
     super(url, method);
-    AllowListRoute.add(url as string); // todo: rewrite later - singleton in object
+    AllowListForRoutes.add(url as string); // todo: rewrite later - singleton in object
   }
 }
 
